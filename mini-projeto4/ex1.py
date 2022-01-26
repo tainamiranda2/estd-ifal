c2dcf0a6301519ccc0f647b2189218ade74a623d
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-01-26 11:15:08 +0000

    projeto 4

--- mini-projeto4/ex1.py
+++ mini-projeto4/ex1.py
@@ -1,40 +1,68 @@
+uma_lista = [9,8,7,3,4, 12, 80,2,1]
+
+def ordenacao(verificar):
+  if len(uma_lista)< 32:
+
+    selectionSort(uma_lista)
+    print("selectionSort", uma_lista)
+  else:
+    mergeSort(uma_lista)
+print("megasort",uma_lista)
+
+
+def selectionSort(lista):
+
+     for index in range (0, len(uma_lista)):
+       min_index=index
+
+       for right in range(index + 1, len(uma_lista)):
+         if uma_lista[right] < uma_lista[min_index]:
+           min_index=right
+
+       uma_lista[index], uma_lista[min_index] = uma_lista[min_index], uma_lista[index]
+ 
+
+#selectionSort(uma_lista)
+#print("selectionSort", uma_lista)
 
 def mergeSort(uma_lista):
-  print("Splitting ",uma_lista)
-  if len(uma_lista)>1:
-    meio = len(uma_lista)//2
-    esquerda = uma_lista[:meio]
-    direita = uma_lista[meio:]
-    mergeSort(esquerda)
-    mergeSort(direita)
-## aqui vai o merge. ver próximo slide.
-  print("Merging ",uma_lista)
-uma_lista = [54,26,93,17,77,31,44,55,20, 
- 1,2,3,4,5,6,7,8,9,10, 
- 14, 13,14,15,16,17,18,99,100,
-  101,102,103, 300 ]
-mergeSort(uma_lista)
-print(uma_lista)
 
-def mergeSort(uma_lista):
-#...
-  i=0
-  j=0
-  k=0
-  while i < len(esquerda) and j < len(direita):
-    if esquerda[i] < direita[j]:
-      uma_lista[k]=esquerda[i]
-      i=i+1
-    else:
-      uma_lista[k]=direita[j]
-      j=j+1
-      k=k+1
-  while i < len(esquerda):
-      uma_lista[k]=esquerda[i]
-      i=i+1
-      k=k+1
-  while j < len(direita):
-      uma_lista[k]=direita[j]
-      j=j+1
-      k=k+1
-print("Merging ",uma_lista)
+    if len(uma_lista) > 1:
+        mid = len(uma_lista) // 2
+        left = uma_lista[:mid]
+        right = uma_lista[mid:]
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              uma_lista[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                uma_lista[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            uma_lista[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            uma_lista[k]=right[j]
+            j += 1
+            k += 1
+
